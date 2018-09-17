e38aa4bd627542577149c27a1dd4bf04af3d02e9
Ali Hamza <ali.hamza4010424@gmail.com>
2018-09-17 17:50:37 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import Text from './components/text/Text';
+import AddContact from './components/AddContact';
+import EditContact from './components/EditContact';
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      //basename= {process.eventNames.PUBLIC_URL} is not working
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/text" component={Text} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/AddContact.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Consumer } from '../context';
+import TextInputGroup from './TextInputGroup';
+import axios from 'axios';
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
+  };
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    const newContacts = {
+      name,
+      email,
+      phone
+    };
+    //Check for Error
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
+
+    const res = await axios.post(
+      'https://jsonplaceholder.typicode.com/users',
+      newContacts
+    );
+    this.setState(dispatch({ type: 'ADD_CONTACT', payload: res.data }));
+
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+    this.props.history.push('/');
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  onPhoneChange = e => this.setState({ phone: e.target.value });
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter name..."
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email..."
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    type="number"
+                    placeholder="Enter Phone..."
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+                  <input
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-primary btn-block"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default AddContact;
